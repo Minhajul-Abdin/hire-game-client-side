428acd7f20a673272458b8b848a3a37546d983da
Minhajul Abdin <minhajul000abdin@gmail.com>
2026-06-15 02:50:34 +0600

    added company and jobs info page

---
+++ src/app/dashboard/recruiter/company/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CompanyPage = () => {
+  return (
+    <div>
+      <h2 className="p-5 text-3xl font-semibold">
+        Company info is not yet shown sorry for the inconvenience
+      </h2>
+    </div>
+  );
+};
+
+export default CompanyPage;
